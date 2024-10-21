06e56c5af69a91a1f317739610412cb48e656a66
Ayesha Siddika <ayeshasiddika777697@gmail.com>
2024-10-21 10:04:50 +0600

    mark countries display funtionality added

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,14 @@ function App() {
       
       <h1>load data all countries{countries.length}</h1>
       <div>visited countries {markCountries.length}</div>
+      <ul style={{display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)', marginLeft: '10px'}}>
+        {markCountries.map(country=>(
+          <div>
+            <img src={country.flags.png} style={{width: '80%', height: '100px' }}></img>
+            <h3>{country.name.common}</h3>
+          </div>
+          ) )}
+      </ul>
       <div className='country-container'>
 
       {countries.map((country)=><Country handleMarkCounries={handleMarkCounries} key={countries.ccn3} country={country}></Country>)
